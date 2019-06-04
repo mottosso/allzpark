c637fce43d315f7feb2c4b9a1ef6743edace3926
marcus <marcus@studioanima.co.jp>
2019-06-04 14:34:14 +0100

    Add convenience startup script

---
+++ launchapp2.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:: Do not affect parent shell
+:: Without this, the below variables are
+:: written to the parent shell
+setlocal
+
+:: These can be overridden
+if "%REZ_PACKAGES_PATH%"=="" (set REZ_PACKAGES_PATH=%USERPROFILE%\packages)
+
+:: Without this, resolving contexts may take seconds to minutes
+if "%REZ_MEMCACHED_URI%"=="" (set REZ_MEMCACHED_URI=127.0.0.1:11211)
+
+:: Defaults to finding projects in your home directory
+if "%LAUNCHAPP_ROOT%"=="" (set LAUNCHAPP_ROOT=%USERPROFILE%\projects)
+
+:: Supports Python 2.7 and Python 3.6+, along with any version of PySide, PySide2, PyQt4 and PyQt5
+if "%LAUNCHAPP_REQUIRE%"=="" (set LAUNCHAPP_REQUIRE=rez python-2 PySide)
+
+rez env %LAUNCHAPP_REQUIRE% -- python -m launchapp2 --root %LAUNCHAPP_ROOT% --verbose %*
