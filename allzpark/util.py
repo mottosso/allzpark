9872d657650aab904b3eb3070acdbaeeccde0b04
marcus <marcus@studioanima.co.jp>
2019-07-22 09:22:00 +0100

    Cosmetics

--- allzpark/util.py
+++ allzpark/util.py
@@ -202,9 +202,12 @@ def atoi(text):
 
 
 def natural_keys(text):
-    '''
+    """Key for use with sorted(key=) and str.sort(key=)
+
     alist.sort(key=natural_keys) sorts in human order
     http://nedbatchelder.com/blog/200712/human_sorting.html
     (See Toothy's implementation in the comments)
-    '''
+
+    """
+
     return [atoi(c) for c in re.split(r'(\d+)', text)]
