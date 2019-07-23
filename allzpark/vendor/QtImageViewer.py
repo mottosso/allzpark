31ab1919e3b5db7c89df6d8dae4549f537de8d93
marcus <marcus@studioanima.co.jp>
2019-07-23 13:03:22 +0100

    Add vendor package

---
+++ allzpark/vendor/QtImageViewer.py
@@ -0,0 +1,184 @@
+""" QtImageViewer.py: PyQt image viewer widget for a QPixmap in a QGraphicsView scene with mouse zooming and panning.
+
+"""
+
+from .Qt.QtCore import Qt, QRectF, Signal
+from .Qt.QtGui import QImage, QPixmap, QPainterPath
+from .Qt.QtWidgets import QGraphicsView, QGraphicsScene
+
+from .Qt import QtCore, QtGui
+
+
+__author__ = "Marcel Goldschen-Ohm <marcel.goldschen@gmail.com>"
+__version__ = '0.9.0'
+
+
+class QtImageViewer(QGraphicsView):
+    """ PyQt image viewer widget for a QPixmap in a QGraphicsView scene with mouse zooming and panning.
+
+    Displays a QImage or QPixmap (QImage is internally converted to a QPixmap).
+    To display any other image format, you must first convert it to a QImage or QPixmap.
+
+    Some useful image format conversion utilities:
+        qimage2ndarray: NumPy ndarray <==> QImage    (https://github.com/hmeine/qimage2ndarray)
+        ImageQt: PIL Image <==> QImage  (https://github.com/python-pillow/Pillow/blob/master/PIL/ImageQt.py)
+
+    Mouse interaction:
+        Left mouse button drag: Pan image.
+        Right mouse button drag: Zoom box.
+        Right mouse button doubleclick: Zoom to show entire image.
+    """
+
+    # Mouse button signals emit image scene (x, y) coordinates.
+    # !!! For image (row, column) matrix indexing, row = y and column = x.
+    leftMouseButtonPressed = Signal(float, float)
+    rightMouseButtonPressed = Signal(float, float)
+    leftMouseButtonReleased = Signal(float, float)
+    rightMouseButtonReleased = Signal(float, float)
+    leftMouseButtonDoubleClicked = Signal(float, float)
+    rightMouseButtonDoubleClicked = Signal(float, float)
+
+    def __init__(self):
+        QGraphicsView.__init__(self)
+
+        # Image is displayed as a QPixmap in a QGraphicsScene attached to this QGraphicsView.
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)
+        self.setCursor(QtCore.Qt.PointingHandCursor)
+
+        # Store a local handle to the scene's current image pixmap.
+        self._pixmapHandle = None
+
+        # Image aspect ratio mode.
+        # !!! ONLY applies to full image. Aspect ratio is always ignored when zooming.
+        #   Qt.IgnoreAspectRatio: Scale image to fit viewport.
+        #   Qt.KeepAspectRatio: Scale image to fit inside viewport, preserving aspect ratio.
+        #   Qt.KeepAspectRatioByExpanding: Scale image to fill the viewport, preserving aspect ratio.
+        self.aspectRatioMode = Qt.KeepAspectRatio
+
+        # Scroll bar behaviour.
+        #   Qt.ScrollBarAlwaysOff: Never shows a scroll bar.
+        #   Qt.ScrollBarAlwaysOn: Always shows a scroll bar.
+        #   Qt.ScrollBarAsNeeded: Shows a scroll bar only when zoomed.
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+
+        # Stack of QRectF zoom boxes in scene coordinates.
+        self.zoomStack = []
+
+        # Flags for enabling/disabling mouse interaction.
+        self.canZoom = True
+        self.canPan = True
+
+    def hasImage(self):
+        """ Returns whether or not the scene contains an image pixmap.
+        """
+        return self._pixmapHandle is not None
+
+    def clearImage(self):
+        """ Removes the current image pixmap from the scene if it exists.
+        """
+        if self.hasImage():
+            self.scene.removeItem(self._pixmapHandle)
+            self._pixmapHandle = None
+
+    def pixmap(self):
+        """ Returns the scene's current image pixmap as a QPixmap, or else None if no image exists.
+        :rtype: QPixmap | None
+        """
+        if self.hasImage():
+            return self._pixmapHandle.pixmap()
+        return None
+
+    def image(self):
+        """ Returns the scene's current image pixmap as a QImage, or else None if no image exists.
+        :rtype: QImage | None
+        """
+        if self.hasImage():
+            return self._pixmapHandle.pixmap().toImage()
+        return None
+
+    def setImage(self, image):
+        """ Set the scene's current image pixmap to the input QImage or QPixmap.
+        Raises a RuntimeError if the input image has type other than QImage or QPixmap.
+        :type image: QImage | QPixmap
+        """
+        if type(image) is QPixmap:
+            pixmap = image
+        elif type(image) is QImage:
+            pixmap = QPixmap.fromImage(image)
+        else:
+            raise RuntimeError("ImageViewer.setImage: Argument must be a QImage or QPixmap.")
+        if self.hasImage():
+            self._pixmapHandle.setPixmap(pixmap)
+        else:
+            self._pixmapHandle = self.scene.addPixmap(pixmap)
+
+        self._pixmapHandle.setTransformationMode(QtCore.Qt.SmoothTransformation)
+        self.setSceneRect(QRectF(pixmap.rect()))  # Set scene size to image size.
+        self.setRenderHints(QtGui.QPainter.Antialiasing |
+                            QtGui.QPainter.SmoothPixmapTransform)
+
+        self.updateViewer()
+
+    def updateViewer(self):
+        """ Show current zoom (if showing entire image, apply current aspect ratio mode).
+        """
+        if not self.hasImage():
+            return
+        if len(self.zoomStack) and self.sceneRect().contains(self.zoomStack[-1]):
+            self.fitInView(self.zoomStack[-1], Qt.KeepAspectRatio)  # Show zoomed rect (ignore aspect ratio).
+        else:
+            self.zoomStack = []  # Clear the zoom stack (in case we got here because of an invalid zoom).
+            self.fitInView(self.sceneRect(), self.aspectRatioMode)  # Show entire image (use current aspect ratio mode).
+
+    def resizeEvent(self, event):
+        """ Maintain current zoom on resize.
+        """
+        self.updateViewer()
+
+    def mousePressEvent(self, event):
+        """ Start mouse pan or zoom mode.
+        """
+        scenePos = self.mapToScene(event.pos())
+        if event.button() == Qt.LeftButton:
+            if self.canPan:
+                self.setDragMode(QGraphicsView.ScrollHandDrag)
+            self.leftMouseButtonPressed.emit(scenePos.x(), scenePos.y())
+        elif event.button() == Qt.RightButton:
+            if self.canZoom:
+                self.setDragMode(QGraphicsView.RubberBandDrag)
+            self.rightMouseButtonPressed.emit(scenePos.x(), scenePos.y())
+        QGraphicsView.mousePressEvent(self, event)
+
+    def mouseReleaseEvent(self, event):
+        """ Stop mouse pan or zoom mode (apply zoom if valid).
+        """
+        QGraphicsView.mouseReleaseEvent(self, event)
+        scenePos = self.mapToScene(event.pos())
+        if event.button() == Qt.LeftButton:
+            self.setDragMode(QGraphicsView.NoDrag)
+            self.leftMouseButtonReleased.emit(scenePos.x(), scenePos.y())
+        elif event.button() == Qt.RightButton:
+            if self.canZoom:
+                viewBBox = self.zoomStack[-1] if len(self.zoomStack) else self.sceneRect()
+                selectionBBox = self.scene.selectionArea().boundingRect().intersected(viewBBox)
+                self.scene.setSelectionArea(QPainterPath())  # Clear current selection area.
+                if selectionBBox.isValid() and (selectionBBox != viewBBox):
+                    self.zoomStack.append(selectionBBox)
+                    self.updateViewer()
+            self.setDragMode(QGraphicsView.NoDrag)
+            self.rightMouseButtonReleased.emit(scenePos.x(), scenePos.y())
+
+    def mouseDoubleClickEvent(self, event):
+        """ Show entire image.
+        """
+        scenePos = self.mapToScene(event.pos())
+        if event.button() == Qt.LeftButton:
+            self.leftMouseButtonDoubleClicked.emit(scenePos.x(), scenePos.y())
+        elif event.button() == Qt.RightButton:
+            if self.canZoom:
+                self.zoomStack = []  # Clear zoom stack.
+                self.updateViewer()
+            self.rightMouseButtonDoubleClicked.emit(scenePos.x(), scenePos.y())
+        QGraphicsView.mouseDoubleClickEvent(self, event)
